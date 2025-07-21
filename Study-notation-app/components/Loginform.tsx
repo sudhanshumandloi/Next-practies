d5bb0c4afca8a3ca96a5f6102a3df65036040960
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-22 01:48:02 +0530

    added js code for login form

--- Study-notation-app/components/Loginform.tsx
+++ Study-notation-app/components/Loginform.tsx
@@ -1,10 +1,73 @@
-"use client";
+import React, { useState } from "react";
+import toast from "react-toast";
+import {AiOutlineEyeInvisible , AiOutlineEye} from "react-icons/ai"
+import { Link, useNavigate } from "react-router-dom";
 
-export default function Loginform({ setIsLoggedIn }: { setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>> }) {
+const Loginform = ({setIsLoggedIn}) => {
+
+    const navigat = useNavigate();
+
+    const [formData, setFormdata] = useState({email:'', password:'' });
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    function changeHandler(event){
+        setFormdata( prevData => (
+            {
+                ...prevData,
+                [event.target.name]: event.target.value
+            }
+         ) )
+    }
+
+    function submitHandler(event){
+        event.preventDefault();
+        setIsLoggedIn(true);
+        toast.success("Logged in")
+        navigat("/dashbord")
+    }
+
     return (
-        <form>
-            {/* Your login form fields here */}
-            <button type="submit">Login</button>
-        </form>
-    );
+        <div>
+            <form onSubmit={submitHandler}>
+                <label>
+                    <p>
+                        Email Address<sub>*</sub>
+                    </p>
+                    <input required
+                        type="email"
+                        value={formData.email}
+                        onChange={changeHandler}
+                        placeholder="Enter email id"
+                        name="email"
+                    />
+                </label>
+
+                <label>
+                    <p>
+                        Password<sub>*</sub>
+                    </p>
+                    <input required
+                        type={showPassword ? ("text") : ("password")}
+                        value={formData.password}
+                        onChange={changeHandler}
+                        placeholder="Enter email id"
+                        name="password"
+                    />
+
+                    <span onClick={() => setShowPassword((prev) => !prev)}>
+                        {showPassword ? (<AiOutlineEyeInvisible />) : (<AiOutlineEye />)}
+                    </span>
+
+                    <Link to="#">
+                        <p>Forget Password</p>
+                    </Link>
+                </label>
+                 
+                <button>Sign in</button>
+            </form>
+        </div>
+    )
 }
+
+export default Loginform;
