a080fcefad2d61f3fd7237c6d66ef92311304be3
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-21 23:22:15 +0530

    Nevbar is completed

--- Study-notation-app/components/Nevbar.tsx
+++ Study-notation-app/components/Nevbar.tsx
@@ -1,6 +1,66 @@
+"use client";
+import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
+import { toast } from "react-toastify";
+import logo from "@/assets/Logo.svg";
+import Image from "next/image";
 
 export default function Nevbar(){
+    const { isLoggedIn, setIsLoggedIn } = useAuth();
+
     return(
-        <div></div>
+        <div className="flex justify-evenly">
+            <Link href="/">
+                 <Image src={logo} alt='logo' width={160} height={32} loading="lazy" />
+            </Link>
+
+            <nav>
+                <ul className="flex ml-3 gap-3">
+                    <li>
+                        <Link href = "/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link href = "/" >About</Link>
+                    </li>
+                    <li>
+                        <Link href = "/" >Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="flex ml-3 gap-3">
+                { !isLoggedIn &&
+                    <Link href="/login">
+                        <button>
+                            Login
+                        </button>
+                    </Link>
+                }
+                { !isLoggedIn &&
+                    <Link href="/signup">
+                        <button>
+                            Signup
+                        </button>
+                    </Link>
+                }
+                { isLoggedIn &&
+                    <Link href ="/">
+                        <button onClick={() => {
+                            setIsLoggedIn(false);
+                            toast.success("Logged out");
+                        }}>
+                            Log out
+                        </button>
+                    </Link>
+                }
+                { isLoggedIn &&
+                    <Link href ="/dashbord">
+                        <button>
+                            dashbord
+                        </button>
+                    </Link>
+                }
+            </div>
+        </div>
     );
 }
