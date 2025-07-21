757f773ead93b2d4a5e4ab06666ffd486bc2fd51
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-21 23:44:10 +0530

    set data fot use in template file

--- Study-notation-app/components/Template.tsx
+++ Study-notation-app/components/Template.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-export default function Template(){
+
+export default function Template({title, desc1, desc2, image, formtype, setIsLoggedIn}){
     return(
-        <div>Template</div>
+        <div>
+            <div>
+                <h1>{title}</h1>
+            </div>
+        </div>
     );
 }
