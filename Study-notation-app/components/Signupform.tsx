2af93ffab52ac9c98d177cb108e0a96e04583b17
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-22 02:15:57 +0530

    remove the error of toast

--- Study-notation-app/components/Signupform.tsx
+++ Study-notation-app/components/Signupform.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import toast from "react-toastify";
+import {toast} from "react-toastify";
 import {AiOutlineEyeInvisible , AiOutlineEye} from "react-icons/ai"
 import { useRouter } from "next/navigation";
 
