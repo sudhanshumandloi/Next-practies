37becb72bb47a468fc9b7d179175e0a4d335e345
sudhanshu Mandloi <sudhanshumandloi99@gmail.com>
2025-07-23 14:21:58 +0530

    complete the css of template componenet

--- Study-notation-app/components/Template.tsx
+++ Study-notation-app/components/Template.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Signupform from "./Signupform";
 import Loginform from "./Loginform";
 import type { StaticImageData } from "next/image";
+import {FcGoogle} from "react-icons/fc";
 
 interface tempobj {
     title: string;
@@ -16,12 +17,15 @@ interface tempobj {
 
 export default function Template({title, desc1, desc2, image, formtype, setIsLoggedIn}: tempobj){
     return(
-        <div>
-            <div>
-                <h1>{title}</h1>
-                <p>
-                    <span>{desc1}</span>
-                    <span>{desc2}</span>
+        <div className="flex w-11/12 justify-between max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0">
+            <div className="w-11/12 max-w-[450px]">     
+                <h1 className="text-richblack-5 font-semibold text-[1.875rem] leading-[2.375rem]">
+                    {title}
+                </h1>
+                <p className="text-[1.125rem] leading-[1.625rem] mt-4">
+                    <span className="text-richblack-100">{desc1}</span>
+                    <br />
+                    <span className="text-blue-400 italic">{desc2}</span>
                 </p>
 
                 {formtype === "signup" ?
@@ -29,20 +33,33 @@ export default function Template({title, desc1, desc2, image, formtype, setIsLog
                 (<Loginform setIsLoggedIn={setIsLoggedIn} />)
                 }
 
-                <div>
-                    <div></div>
-                    <div>OR</div>
-                    <div></div>
+                <div className="flex w-full items-center my-4 gap-x-2">
+                    <div className="w-full h-[1px] bg-richblack-700"></div>
+                    <div className="text-richblack-700 font-medium leading-[1.375rem]">OR</div>
+                    <div className="w-full h-[1px] bg-richblack-700"></div>
                 </div>
 
-                <button>
+                <button className="w-full flex justify-center items-center rounded-[8px] font-medium 
+                    text-richblack-100 border border-richblack-700 px-[12px] py-[8px] gap-x-2 mt-6">
+                    <FcGoogle />
                     <p>Sign Up with Google</p>
                 </button>
             </div>
 
-            <div>
-                <Image src={frame} alt="frame" height={584} width={558} loading="lazy"/>
-                <Image src={image} alt="student" height={490} width={558} loading="lazy"/>
+            <div className="w-11/12 relative max-w-[450px]">
+                <Image src={frame} 
+                    alt="frame" 
+                    height={584} 
+                    width={558} 
+                    loading="lazy"                    
+                />
+                <Image src={image} 
+                    alt="student" 
+                    height={490} 
+                    width={558} 
+                    loading="lazy"                    
+                    className="absolute -top-4 right-4"
+                />
             </div>
         </div>
     );
